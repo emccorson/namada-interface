299514e1fce4aa1182b5bca55112b7c6d71fe53a
Eric Corson <public@giraffedev.com>
2023-05-19 11:26:48 +0900

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            rustup
+            #wasm-bindgen-cli
+            (callPackage ./wasm-bindgen-cli.nix { })
+            wasm-pack
+            binaryen
+            protobuf
+            yarn
+            nodejs-18_x
+            emscripten #using emcc because I can't get gcc or clang to work
+            nodePackages.web-ext
+          ];
+        };
+      }
+    );
+}
